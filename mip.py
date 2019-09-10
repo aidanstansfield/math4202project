f2b5a1992874564b8226acb6b4926f7ab8eb9e69
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-09-10 14:57:12 +1000

    fixes

--- mip.py
+++ mip.py
@@ -6,12 +6,12 @@ from fractions import Fraction
 numEdges = 19
 numNodes = 15
 K = 1
-maxTime = 100
+maxTime = 50
 
 M = range(0, numEdges)
 N = range(0, numNodes)
 L = range(0, 2 * numEdges)
-T = range(0, maxTime)
+T = range(1, maxTime + 1)
 
 mip = Model("Model searchers searching for a randomly distributed immobile" \
     "target on a unit network")
@@ -25,10 +25,10 @@ arcs = genArcs(graph)
 nodes = genNodes(graph)
 
 X = {(t, l): mip.addVar(vtype=GRB.BINARY) for t in T for l in L}
-Y = {(t, m): mip.addVar(vtype=GRB.BINARY) for t in T for m in M}
-alpha = {t: mip.addVar() for t in T}
+Y = {(t, m): mip.addVar(vtype=GRB.BINARY) for t in range(0, maxTime + 1) for m in M}
+alpha = {t: mip.addVar() for t in range(0, maxTime + 1)}
 
-mip.setObjective(quicksum((alpha[t]+alpha[t + 1])/2 for t in range(0, maxTime - 1)), GRB.MINIMIZE) #2
+mip.setObjective(quicksum((alpha[t - 1] + alpha[t])/2 for t in T), GRB.MINIMIZE) #2
 
 #num arcs searched can't exceed num searchers
 searcherLim = {t: mip.addConstr(quicksum(X[t, l] for l in L) <= K) for t in T}
@@ -38,15 +38,15 @@ defAlpha = {t: mip.addConstr(alpha[t] == 1 - quicksum(p * Y[t, m] for m in
 #update search info after every time step
 updateSearch = {(t, m): mip.addConstr(Y[t, m] <= Y[t - 1, m] + 
                 quicksum(O(arcs[l], edges[m]) * X[t, l] for l in L)) for t in 
-                range(1, maxTime) for m in M}
+                T for m in M}
 #conserve arc flow on nodes
 consFlow = {(t, n): mip.addConstr(quicksum(E(arcs[l], nodes[n]) * X[t, l] for l in L) == 
                       quicksum(S(arcs[l], nodes[n])* X[t + 1, l] for l in L)) for t in 
-                        range(0, maxTime - 1) for n in N}
+                        range(1, maxTime) for n in N}
 #initially, no edges have been searched
 initY = {m: mip.addConstr(Y[0, m] == 0) for m in M}
 #limit alpha so target is found by time maxTime
-mip.addConstr(alpha[maxTime - 1] <= 0.001)
+mip.addConstr(alpha[maxTime] <= 0.001)
 
 mip.optimize()
     
