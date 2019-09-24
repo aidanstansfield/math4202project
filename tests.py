71c15822e992fadc87378a3980a5e63c2db6c553
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2019-09-24 11:39:38 +1000

    Add test for large dense networks

--- tests.py
+++ tests.py
@@ -1,34 +1,41 @@
 import unittest
 from problemGen import generateNetwork
 
+
 class TestGenerateNetwork(unittest.TestCase):
 
     def setUp(self):
-        self.denseNodes = 12
-        self.denseEdges = 28
+        self.smallDenseNodes = 12
+        self.smallDenseEdges = 28
+        self.largeDenseNodes = 150
+        self.largeDenseEdges = 300
         self.sparseNodes = 18
         self.sparseEdges = 24
     
     def testCorrectNodesSparseNetwork(self):
-        network,_,_ = generateNetwork(self.sparseEdges, self.sparseNodes)
+        network, _, _ = generateNetwork(self.sparseEdges, self.sparseNodes)
 
         self.assertEqual(self.sparseNodes, len(network.keys()))
 
     def testCorrectEdgesSparseNetwork(self):
-        network,_,_ = generateNetwork(self.sparseEdges, self.sparseNodes)
+        network, _, _ = generateNetwork(self.sparseEdges, self.sparseNodes)
 
         self.assertEqual(self.sparseEdges, sum(len(network[n]) for n in network.keys())//2)
 
     def testCorrectNodesDenseNetwork(self):
-        network,_,_ = generateNetwork(self.denseEdges, self.denseNodes)
+        network, _, _ = generateNetwork(self.smallDenseEdges, self.smallDenseNodes)
 
-        self.assertEqual(self.denseNodes, len(network.keys()))
+        self.assertEqual(self.smallDenseNodes, len(network.keys()))
 
-    def testCorrectEdgesDenseNetwork(self):
-        network,_,_ = generateNetwork(self.denseEdges, self.denseNodes)
+    def testCorrectEdgesSmallDenseNetwork(self):
+        network, _, _ = generateNetwork(self.smallDenseEdges, self.smallDenseNodes)
 
-        self.assertEqual(self.denseEdges, sum(len(network[n]) for n in network.keys())//2)
+        self.assertEqual(self.smallDenseEdges, sum(len(network[n]) for n in network.keys())//2)
+    
+    def testCorrectEdgesLargeDenseNetwork(self):
+        network, _, _ = generateNetwork(self.largeDenseEdges, self.largeDenseNodes)
 
+        self.assertEqual(self.largeDenseEdges, sum(len(network[n]) for n in network.keys())//2)
 
 
 if __name__ == "__main__":
