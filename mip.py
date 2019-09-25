bc806dff7186fd5faa2fe72e0ae1f14e5a2ab91f
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-09-25 14:43:39 +1000

    some micro changes of formulation

--- mip.py
+++ mip.py
@@ -22,11 +22,11 @@ def MIP(probType, K, numEdges, numNodes, seed=None):
     #min time for searchers to search every arc
     maxTime = 50
     print('yes')
-#    graph, p, edges = genEx(probType)
-#    print('yes2')
-#    #sets
-#    numEdges = len(edges)
-#    numNodes = len(graph.keys())
+    #    graph, p, edges = genEx(probType)
+    #    print('yes2')
+    #    #sets
+    #    numEdges = len(edges)
+    #    numNodes = len(graph.keys())
     M = range(0, numEdges)
     N = range(0, numNodes)
     L = range(0, 2 * numEdges)
@@ -35,7 +35,7 @@ def MIP(probType, K, numEdges, numNodes, seed=None):
     #data
     #gen network - p is pdf and edges is set of edges
     graph, p, edges, graphSeed = generateNetwork(numEdges, numNodes, probType, seed)
-#    displayGraph(graph)
+    #    displayGraph(graph)
     S = {}
     E = {}
     O = {}
@@ -63,10 +63,10 @@ def MIP(probType, K, numEdges, numNodes, seed=None):
                 S[l, n] = 0
                 E[l, n] = 0
     
-#    endgen = clock()
-#    genTime = startgen - endgen
+    #    endgen = clock()
+    #    genTime = startgen - endgen
     
-#    startMIP = clock()
+    #    startMIP = clock()
     
     mip = Model("Model searchers searching for a randomly distributed immobile" \
                 "target on a unit network")
@@ -77,18 +77,19 @@ def MIP(probType, K, numEdges, numNodes, seed=None):
     alpha = {t: mip.addVar() for t in T}
     
     #objective
-    mip.setObjective(quicksum((alpha[t - 1]+alpha[t])/2 for t in T[1:]), GRB.MINIMIZE)
+    mip.setObjective(quicksum((alpha[t-1]+alpha[t])/2 for t in T[1:]), GRB.MINIMIZE)
     
     #constraints
     
     #num arcs searched can't exceed num searchers- implicitly defines capacity
-    searcherLim = {t: mip.addConstr(quicksum(X[t, l] for l in L) == K) for t in T[1:]}
+    searcherLim = {t: mip.addConstr(quicksum(X[t, l] for l in L) <= K) for t in T[1:]}
     #capacity of flow on each arc
-#    lowerBound = {(t, l): mip.addConstr(X[t, l] >= 0) for t in T[1:] for l in L}
+    #    lowerBound = {(t, l): mip.addConstr(X[t, l] >= 0) for t in T[1:] for l in L}
     #lower bound on flow
     #define alpha as in paper
     defAlpha = {t: mip.addConstr(alpha[t] == 1 - quicksum(p[edges[m]] * Y[t, m] for m in 
                 M)) for t in T}
+    #mip.addConstr(alpha[0] == 1)
     #update search info after every time step
     updateSearch = {(t, m): mip.addConstr(Y[t, m] <= Y[t - 1, m] + 
                     quicksum(O[l, m] * X[t, l] for l in L)) for t in 
@@ -96,7 +97,7 @@ def MIP(probType, K, numEdges, numNodes, seed=None):
     #conserve arc flow on nodes
     consFlow = {(t, n): mip.addConstr(quicksum(E[l, n] * X[t, l] for l in L) == 
                           quicksum(S[l, n]* X[t + 1, l] for l in L)) for t in 
-                            T[1: -2] for n in N}
+                            T[1: -1] for n in N}
     #initially, no edges have been searched
     initY = {m: mip.addConstr(Y[0, m] == 0) for m in M}
     #limit y so that every arc is searched by T
@@ -111,7 +112,7 @@ def MIP(probType, K, numEdges, numNodes, seed=None):
     
     return mip, graph, time
     
-mip, graph, _ = MIP(UNIFORM, 20, 100, 50)
+mip, graph, _ = MIP(UNIFORM, 1, 19, 15)
 
 #ob = [0 for i in range(10)]
 #time = [0 for i in range(10)]
