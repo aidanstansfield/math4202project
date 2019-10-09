31485981d650f22c9d2b4f1ef3675d513f11a9a0
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-10-09 13:04:04 +1000

    Major improvements to my colgen, still just strictly worse tho :( Also I copied the MIP out into main for the sake of debugging, change the flag to True if you want to be able to reference variables in interactive

--- mip.py
+++ mip.py
@@ -1,5 +1,5 @@
 from gurobipy import quicksum, GRB, Model
-from problemGen import generateNetwork, genArcs, genNodes, displayGraph, indexToXY
+from problemGen import generateNetwork, genArcs, genNodes, displayGraph, indexToXY, genLeaf
 from time import clock
 from matplotlib import pyplot, animation
 import msvcrt
@@ -84,6 +84,7 @@ def MIP(probType, K, numEdges, numNodes, maxTime, graph = None, edges = None, p 
 #    print(arcs)
     # gen set of nodes
     nodes = genNodes(graph)
+    leafs = genLeaf(graph)
 #    print('E: ', edges)
     # define values for functions S, E and O and store in dict.
     for l in L:
@@ -138,6 +139,9 @@ def MIP(probType, K, numEdges, numNodes, maxTime, graph = None, edges = None, p 
     # limit y so that every edge is searched by T
     {m: mip.addConstr(Y[maxTime, m] == 1) for m in M}
     
+    # must use at least 1 leaf if uniform
+    if probType == UNIFORM and len(leafs) != 0:
+        mip.addConstr(quicksum(X[1, arcs.index(leaf)] for leaf in leafs) >= 1)
     
     
     # Changinge Branch priority based on aggregation
@@ -156,14 +160,110 @@ def MIP(probType, K, numEdges, numNodes, maxTime, graph = None, edges = None, p 
         "L": L,
         "maxTime": maxTime
     }
-#    visualiseStrategy(state, arcs, graph)
+    visualiseStrategy(state, arcs, graph)
 
     return mip, graph, time#, X, p, edges, O, arcs, L, M, T, alpha
 
 
 if __name__ == "__main__":
-    mip, graph, _ = MIP(UNIFORM, 1, 19, 15, 2*19)
+    if True:
+        mip, graph, _ = MIP(UNIFORM, 1, 19, 15, 2*19, seed=748345644471475368)
+    else:
+        numEdges = 19
+        numNodes = 15
+        seed = 748345644471475368
+        K = 2
+        maxTime = 2 * numEdges
+        probType = 0
+        M = range(0, numEdges)
+        N = range(0, numNodes)
+        L = range(0, 2 * numEdges)
+        T = range(0, maxTime + 1)
+    
+        #data
+        #gen network - p is pdf and edges is set of edges
+        graph, p, edges, _ = generateNetwork(numEdges, numNodes, probType, seed)
+        S = {}
+        E = {}
+        O = {}
+        # gen set of arcs
+        arcs = genArcs(graph)
+        # gen set of nodes
+        nodes = genNodes(graph)
+        leafs = genLeaf(graph)
+        for l in L:
+            for m in M:
+                if arcs[l] == edges[m] or arcs[l] == edges[m][::-1]:
+                    O[l, m] = 1
+                else:
+                    O[l, m] = 0
+            for n in N:
+                if arcs[l][0] == nodes[n]:
+                    S[l, n] = 1
+                    E[l, n] = 0
+                elif arcs[l][1] == nodes[n]:
+                    E[l, n] = 1
+                    S[l, n] = 0
+                else:
+                    S[l, n] = 0
+                    E[l, n] = 0
+        
+        mip = Model("Model searchers searching for a randomly distributed immobile" \
+                    "target on a unit network")
+        
+        # variables
+        X = {(t, l): mip.addVar(vtype=GRB.BINARY) for t in T[1:] for l in L}
+        Y = {(t, m): mip.addVar(vtype=GRB.BINARY) for t in T for m in M}
+        alpha = {t: mip.addVar() for t in T}
+        
+        # objective
+        mip.setObjective(quicksum((alpha[t-1]+alpha[t])/2 for t in T[1:]), GRB.MINIMIZE)
+        
+        # constraints
+        # num arcs searched can't exceed num searchers- implicitly defines capacity
+        searcherLim = {t: mip.addConstr(quicksum(X[t, l] for l in L) <= K) for t in T[1:]}
 
+        # define alpha as in paper
+        defAlpha = {t: mip.addConstr(alpha[t] == 1 - quicksum(p[edges[m]] * Y[t, m] for m in 
+                    M)) for t in T}
+        # mip.addConstr(alpha[0] == 1)
+        # update search info after every time step
+        updateSearch = {(t, m): mip.addConstr(Y[t, m] <= Y[t - 1, m] + 
+                        quicksum(O[l, m] * X[t, l] for l in L)) for t in 
+                        T[1:] for m in M}
+        # conserve arc flow on nodes
+        consFlow = {(t, n): mip.addConstr(quicksum(E[l, n] * X[t, l] for l in L) == 
+                              quicksum(S[l, n]* X[t + 1, l] for l in L)) for t in 
+                                T[1: -1] for n in N}
+        # initially, no edges have been searched
+        initY = {m: mip.addConstr(Y[0, m] == 0) for m in M}
+        # limit y so that every edge is searched by T
+        {m: mip.addConstr(Y[maxTime, m] == 1) for m in M}
+        
+        # must use at least 1 leaf if uniform
+        if probType == UNIFORM and len(leafs) != 0:
+            mip.addConstr(quicksum(X[1, arcs.index(leaf)] for leaf in leafs) >= 1)
+        
+        
+        
+        # Changinge Branch priority based on aggregation
+    #    XT = mip.addVar(vtype=GRB.INTEGER)
+    #    mip.addConstr(XT==quicksum(X.values()))
+    #    XT.BranchPriority = 10
+    #    mip.setParam('GURO_PAR_MINBPFORBID', 1)
+        
+        mip.optimize()
+        time = mip.Runtime
+    
+        state = {
+            "X": X,
+            "Y": Y,
+            "T": T,
+            "L": L,
+            "maxTime": maxTime
+        }
+        visualiseStrategy(state, arcs, graph)
+        
 #mip, graph, _ = MIP(UNIFORM, 3, 19, 15, 25)
 
 
