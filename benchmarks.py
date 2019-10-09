3fcc179e4af20e8bf99aea680502ca1cba387c03
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2019-10-09 18:32:11 +1000

    Write results to file more
    Fix instance generation to use same seed for graphs

--- benchmarks.py
+++ benchmarks.py
@@ -12,15 +12,23 @@ def generateProblems(classes, num):
     for c in classes:
         edges, nodes = parseClass(c)
         for graph in range(num):
+            graphSeed = None
             for pType in ['Uniform', 'Non-Uniform']:
                 probType = 0 if (pType == 'Uniform') else 1
-                graph, prob, _, seed = generateNetwork(edges, nodes, probType)
-                writeGraph(graph, seed, prob, './problemInstances/'+c+'/'+pType+'/')
+                graph, prob, _, graphSeed = generateNetwork(edges, nodes, probType, graphSeed)
+                writeGraph(graph, graphSeed, prob, './problemInstances/'+c+'/'+pType+'/')
 
+def displayResults(results):
+    
+    
+    
 if __name__ == '__main__':
-    classes = ['M19N15', 'M24N18', 'M20N15', 'M20N8', 'M30N22', 'M30N12',
-               'M30N9', 'M40N38', 'M40N16', 'M40N11', 'M50N35', 'M50N20',
-               'M50N14']
+#    classes = ['M19N15', 'M24N18', 'M20N15', 'M20N8', 'M30N22', 'M30N12',
+#               'M30N9', 'M40N38', 'M40N16', 'M40N11', 'M50N35', 'M50N20',
+#               'M50N14']
+    
+    classes = ['M19N15', 'M24N18']
+    
     instances = 10
     # generateProblems(classes, instances)
     UNIFORM = 0
@@ -29,17 +37,24 @@ if __name__ == '__main__':
     results = {}
     for c in classes:
         numEdges, numNodes = parseClass(c)
-        for k in range(1, 6):
+        for k in range(1, 3):
             for pType in ['Uniform', 'Non-Uniform']:
                 path = './problemInstances/'+c+'/'+pType+'/'
                 count = 0
                 for file in os.listdir(path):
+                    try :
+                        graph, prob = readGraph(path+file)
+                    #Found a results.txt, so skip it
+                    except SyntaxError:
+                        print("skipped", file)
+                        continue
                     count+=1
-                    graph, prob = readGraph(path+file)
+                    
                     # ProbType param doesn't matter since p is passed in
-                    mip, graph, _ = MIP(UNIFORM, k, numEdges, numNodes, 2*numEdges, graph=graph, p=prob)
+                    mip, graph, _ = MIP(UNIFORM, k, numEdges, numNodes, 2*numEdges//k, graph=graph, p=prob)
                     results[c, pType, k, count] = (mip.objVal, mip.RunTime, mip.MipGap)
-    
-    with open('results.txt', 'w') as f:
-        f.write(str(results))
+                    print('Complete', c, pType, "searchers:", k)
+                
+                with open(path+'results.txt', 'w') as f:
+                    f.write(str(results))
     print(results)
