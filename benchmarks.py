a41befc1b926ccae4813ef7ca67dc9e44a4a9e67
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2019-10-10 17:01:35 +1000

    Add function to output results as LaTeX table

--- benchmarks.py
+++ benchmarks.py
@@ -45,49 +45,76 @@ def displayResults(results, classes, maxSearchers, instances):
                 print('Avg Gap', gapTotal/instances)
                 print()
 
+def displayLatexFormat(results, classes, maxSearchers, instances):
+    for val in range(3): # Make a table for each return value (objective,  runtime and mipgap)
+        for c in classes:
+            print('\n\\begin{table}')
+            print('\\footnotesize')
+            print(c)
+            print('\\begin{tabular}{', '|c|'*(instances+2), ' }')
+            print('\hline')
+            print('Scenario', end=' ')
+            for i in range(1,instances+1):
+                print('& ', i, sep='', end=' ')
+            print('& Average\\\\')
+            print('\hline')
+            for k in range(1, maxSearchers+1):
+                for pType in ['Uniform', 'Non-Uniform']:
+                    print("K", k, "-", pType, sep='', end=' ')
+                    totals = [0, 0, 0]
+                    for i in range(1, instances+1):
+                        
+                        print('& ', round(results[c, pType, k, i][val], 3), sep='', end = ' ')
+                        totals[val] += results[c, pType, k, i][val]
+                    print(round(totals[val]/instances, 3), '\\\\ \hline', sep='')
+            print('\end{tabular}')
+            print('\end{table}')
+
 
 if __name__ == '__main__':
     #    classes = ['M19N15', 'M24N18', 'M20N15', 'M20N8', 'M30N22', 'M30N12',
     #               'M30N9', 'M40N38', 'M40N16', 'M40N11', 'M50N35', 'M50N20',
     #               'M50N14']
 
-    classes = ['M19N15', 'M24N18']
+    classes = ['M19N15']#, 'M24N18']
 
     instances = 10
     # generateProblems(classes, instances)
     UNIFORM = 0
     NON_UNIFORM = 1
 
-    with open('NonSameResults_M19N15_M25N18.txt', 'r') as f:
+    with open('M19N15results.txt', 'r') as f:
         results = eval(f.read())
 
-#    displayResults(results, classes, 3, 10)
-    results = {}
-    for c in classes:
-        numEdges, numNodes = parseClass(c)
-        for k in range(2, 0, -1):
-             for pType in ['Uniform', 'Non-Uniform']:
-                 path = './problemInstances/'+c+'/'+pType+'/'
-                 count = 0
-                 for file in os.listdir(path):
-                     try:
-                         graph, prob = readGraph(path+file)
-                     # Found a results.txt, so skip it
-                     except SyntaxError:
-                         print("skipped", file)
-                         continue
-                     count += 1
-
-                     # ProbType param doesn't matter since p is passed in
-                     mip, graph, _ = MIP(
-                         UNIFORM, k, numEdges, numNodes, 2*numEdges//k, graph=graph, p=prob)
-                     
-                     results[c, pType, k, count] = (mip.objVal, mip.RunTime, mip.MipGap)
-                     
-                     print((c, pType, k, count), ":", (mip.objVal, mip.RunTime, mip.mipGap))
-                     print('Complete', c, pType, "searchers:", k)
-
-                 with open(path+'results.txt', 'w') as f:
-                     f.write(str(results))
-    print(results)
-    displayResults(results, classes, 2, instances)
+#    displayResults(results, classes, 2, 10)
+    
+    displayLatexFormat(results, classes, 2, instances)
+#    results = {}
+#    for c in classes:
+#        numEdges, numNodes = parseClass(c)
+#        for k in range(2, 0, -1):
+#             for pType in ['Uniform', 'Non-Uniform']:
+#                 path = './problemInstances/'+c+'/'+pType+'/'
+#                 count = 0
+#                 for file in os.listdir(path):
+#                     try:
+#                         graph, prob = readGraph(path+file)
+#                     # Found a results.txt, so skip it
+#                     except SyntaxError:
+#                         print("skipped", file)
+#                         continue
+#                     count += 1
+#
+#                     # ProbType param doesn't matter since p is passed in
+#                     mip, graph, _ = MIP(
+#                         UNIFORM, k, numEdges, numNodes, 2*numEdges//k, graph=graph, p=prob)
+#                     
+#                     results[c, pType, k, count] = (mip.objVal, mip.RunTime, mip.MipGap)
+#                     
+#                     print((c, pType, k, count), ":", (mip.objVal, mip.RunTime, mip.mipGap))
+#                     print('Complete', c, pType, "searchers:", k)
+#
+#                 with open(path+'results.txt', 'w') as f:
+#                     f.write(str(results))
+#    print(results)
+#    displayResults(results, classes, 2, instances)
